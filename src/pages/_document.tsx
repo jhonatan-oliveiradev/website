c375bb4478a55376052aeab153f787c4b0af1b20
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-15 20:31:52 -0300

    :sparkles: config inicial

---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						rel="shortcut icon"
+						href="../../public/favicon.png"
+						type="image/png"
+					/>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest"></link>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
