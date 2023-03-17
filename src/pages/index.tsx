b8b50d9f8d9ba391db9521b0df023fafd7eee4d3
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-16 22:43:38 -0300

    :construction: feat: dados dinâmicos em Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Image from "next/image";
+import { getPrismicClient } from "../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import techsImage from "../../public/images/techs.svg";
 
-export default function Home() {
+type Content = {
+	title: string;
+	titleContent: string;
+	linkAction: string;
+	mobileTitle: string;
+	mobileContent: string;
+	mobileBanner: string;
+	titleWeb: string;
+	webContent: string;
+	webBanner: string;
+};
+
+interface ContentProps {
+	content: Content;
+}
+
+export default function Home({ content }: ContentProps) {
 	return (
 		<>
 			<Head>
@@ -14,46 +34,44 @@ export default function Home() {
 			<main className={styles.container}>
 				<div className={styles.containerHeader}>
 					<section className={styles.ctaText}>
-						<h1>Levando você ao próximo nível!</h1>
-						<span>
-							Uma plataforma com cursos que vão do zero até o profissional na
-							prática, direto ao ponto aplicando o que usamos no mercado de
-							trabalho. 👊
-						</span>
-						<a>
+						<h1>{content.title}</h1>
+						<span>{content.titleContent}</span>
+						<a href={content.linkAction}>
 							<button>COMEÇAR AGORA</button>
 						</a>
 					</section>
-					<img src="/images/banner-conteudos.png" alt="Hero" />
+					<img
+						src="/images/banner-conteudos.png"
+						alt={content.title}
+						title={content.title}
+					/>
 				</div>
 
 				<hr className={styles.divisor} />
 
 				<div className={styles.sectionContent}>
 					<section>
-						<h2>Aprenda a criar aplicativos para Android e iOS</h2>
-						<span>
-							Você vai descobrir o jeito mais moderno de desenvolver apps
-							nativos para iOS e Android, construindo aplicativos do zero até
-							aplicativos.
-						</span>
+						<h2>{content.mobileTitle}</h2>
+						<span>{content.mobileContent}</span>
 					</section>
 
 					<img
-						src="/images/financasApp.png"
-						alt="Conteúdos desenvolvimento de apps"
+						src={content.mobileBanner}
+						alt={content.mobileTitle}
+						title={content.mobileTitle}
 					/>
 				</div>
 
 				<hr className={styles.divisor} />
 				<div className={styles.sectionContent}>
-					<img src="/images/webDev.png" alt="Conteúdos desenvolvimento web" />
+					<img
+						src={content.webBanner}
+						alt={content.titleWeb}
+						title={content.titleWeb}
+					/>
 					<section>
-						<h2>Aprenda criar sistemas web</h2>
-						<span>
-							Criar sistemas web, sites usando as tecnologias mais modernas e
-							requisitadas pelo mercado.
-						</span>
+						<h2>{content.titleWeb}</h2>
+						<span>{content.webContent}</span>
 					</section>
 				</div>
 
@@ -66,7 +84,7 @@ export default function Home() {
 					<span>
 						E você vai perder a chance de evoluir de uma vez por todas?
 					</span>
-					<a>
+					<a href={content.linkAction}>
 						<button>ACESSAR TURMA</button>
 					</a>
 				</div>
@@ -74,3 +92,40 @@ export default function Home() {
 		</>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const prismic = getPrismicClient();
+
+	const response = await prismic.query([
+		Prismic.Predicates.at("document.type", "home"),
+	]);
+
+	const {
+		title,
+		subtitle,
+		link_action,
+		mobile,
+		mobile_content,
+		mobile_banner,
+		title_web,
+		web_content,
+		web_banner,
+	} = response.results[0].data;
+
+	const content = {
+		title: RichText.asText(title),
+		titleContent: RichText.asText(subtitle),
+		linkAction: link_action.url,
+		mobileTitle: RichText.asText(mobile),
+		mobileContent: RichText.asText(mobile_content),
+		mobileBanner: mobile_banner.url,
+		titleWeb: RichText.asText(title_web),
+		webContent: RichText.asText(web_content),
+		webBanner: web_banner.url,
+	};
+
+	return {
+		props: { content },
+		revalidate: 60 * 60 * 24, // 24 hours
+	};
+};
